b8fba630d31369cd2ae64c0dd537c12e54ecc399
dcp1990 <dcp1990>
2005-06-12 15:14:14 +0000

    Add for pcre

--- global.mk
+++ global.mk
@@ -1,4 +1,6 @@
 # global stuff
-# $Amigan: phoned/global.mk,v 1.2 2005/06/01 00:43:07 dcp1990 Exp $
+# $Amigan: phoned/global.mk,v 1.3 2005/06/12 15:14:14 dcp1990 Exp $
 CC=cc
 PREFIX=/usr/local
+CPPFLAGS=-I/usr/local/include
+LDFLAGS=-L/usr/local/lib
